bbef4300e191ffb9b497379a5b1218781c694b0f
vinayctpl <76211107+vinayctpl@users.noreply.github.com>
2020-12-29 10:15:30 +0530

    Farming Application
    This is application from which client, who uses this application has knowledge about how much plant he has to grow and in how much area, so that it can fulfill its requirements.

---
+++ FarmingApp/FarmingApp/DataEntry.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.Json;
+
+namespace FarmingApp
+{
+    /// <summary>
+    /// Interaction logic for DataEntry.xaml
+    /// </summary>
+    public partial class DataEntry : Window
+    {
+        string _DataFilePath;
+        public DataEntry()
+        {
+            InitializeComponent();
+            _DataFilePath = System.IO.Path.GetFullPath(".") + "\\data.txt";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FileStream fs = null ;
+            StreamWriter sw = null; ;
+            string nameOfPlant = txtNameOfPlant.Text;
+            string distanceBetweenPlants = txtDistanceBtwPlants.Text;
+            string yieldPeriod = txtYeildPeriod.Text;
+            string growingPeriod = txtGrowingPeriod.Text;
+            string plantTimeInNursery = txtTimeInNursery.Text;
+            string plantProductivity = txtPlantProductivity.Text;
+            fs = new FileStream(_DataFilePath, FileMode.Append, FileAccess.Write);
+            sw = new StreamWriter(fs);
+            string fileString = nameOfPlant + "," + distanceBetweenPlants + "," + yieldPeriod + "," + growingPeriod + "," + plantTimeInNursery+","+plantProductivity+"\r\n";
+            sw.WriteLine(fileString);
+            txtNameOfPlant.Text = "";
+            txtDistanceBtwPlants.Text = "";
+            txtYeildPeriod.Text = "";
+            txtGrowingPeriod.Text = "";
+            txtTimeInNursery.Text = "";
+            txtPlantProductivity.Text = "";
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+        private void txtNameOfPlant_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FarmingApp/FarmingApp/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace FarmingApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string _DataFilePath;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _DataFilePath = System.IO.Path.GetFullPath(".") + "\\data.txt";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double distanceBtwPlants, numberOfPlantsRequired, minAreaRequired, yeildPeriod, growingPeriod, plantTimeInNursery;
+            double totalLifespan,plantProductivity;
+            double afterFirst;
+            string nameOfPlant = txtNameOfPlant.Text;
+            string requirement = txtRequirement.Text;
+            if (nameOfPlant=="" || nameOfPlant.Length==0)
+            {
+                // throw an error
+            }
+            if(requirement=="" || requirement.Length==0)
+            {
+                //throw an error
+            }
+            bool isExists = false;
+            string requiredLine = "";
+            string[] lines = File.ReadAllLines(_DataFilePath);
+            foreach(string line in lines)
+            {
+                string[] words = line.Split(",");
+                if (words[0] == nameOfPlant)
+                {
+                    isExists = true;
+                    requiredLine = line;
+                }
+            }
+            if(isExists==false)
+            {
+                // throw an error
+            }
+            //distanceBtwPlants = double.Parse(txtDistanceBtwPlants.Text);
+            if(requiredLine.Length==0 || requiredLine=="")
+            {
+                //throw an error
+                //this case will not occur but in very stange condition it can occur, so for precaution I am taking this case
+            }
+            string[] requiredLines = requiredLine.Split(",");
+            distanceBtwPlants = double.Parse(requiredLines[1]);
+            yeildPeriod = double.Parse(requiredLines[2]);
+            growingPeriod = double.Parse(requiredLines[3]);
+            plantTimeInNursery = double.Parse(requiredLines[4]);
+            plantProductivity = double.Parse(requiredLines[5]);
+            //System.Diagnostics.Trace.WriteLine(yeildPeriod + " " + growingPeriod);
+
+            numberOfPlantsRequired = Math.Ceiling((double.Parse(requirement) * yeildPeriod)/plantProductivity);
+            minAreaRequired = (numberOfPlantsRequired * Math.Pow(distanceBtwPlants,2))/Math.Pow(12,2);
+            totalLifespan = yeildPeriod + growingPeriod;
+            afterFirst = yeildPeriod + plantTimeInNursery + 1;
+            txtPlants.Content = numberOfPlantsRequired+" Plants Required";
+            txtArea.Content = minAreaRequired.ToString("0.00")+" feet Area Required";
+            txtTotalLifeSpan.Content = totalLifespan + " weeks life span";
+            txtAfterFirst.Content = afterFirst + " weeks put next plant from nursery into field";
+        }
+
+        private void DataEntry_Button_Click(object sender, RoutedEventArgs e)
+        {
+            DataEntry de = new DataEntry();
+            de.Show();
+        }
+    }
+}
